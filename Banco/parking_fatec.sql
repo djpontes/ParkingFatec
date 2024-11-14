75090bf346c2ad04fcc1fdc3a19c1f2746b1d9b0
djpontes <dudaspontesjaque@gmail.com>
2024-11-14 20:06:55 -0300

    Atualização do banco (se deus quiser a ultima)

--- Banco/parking_fatec.sql
+++ Banco/parking_fatec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12/11/2024 às 23:38
+-- Tempo de geração: 14/11/2024 às 00:58
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -33,6 +33,13 @@ CREATE TABLE `estacionamento` (
   `qntd_vagas_carro` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `estacionamento`
+--
+
+INSERT INTO `estacionamento` (`id`, `qntd_vagas_moto`, `qntd_vagas_carro`) VALUES
+(1, 27, 29);
+
 -- --------------------------------------------------------
 
 --
@@ -60,21 +67,57 @@ INSERT INTO `motoristas` (`id`, `nome`, `email`, `cnh`, `ra_rm`, `telefone`, `ti
 (3, 'Film', 'film@gmail.com', '33333333333', '3333333333333', '(33)33333-3333', '0', 1),
 (4, 'Fay', 'fay@fatec.sp.gov.br', '55555555555', '5555555555555', '(55)55555-5555', '0', 1),
 (5, 'May', 'may@fatec.sp.gov.br', '66666666666', '6666666666666', '(66)66666-6666', '1', 1),
-(6, 'Orm', 'orm@gmail.com', '77777777777', '6666666666666', '(77)77777-7777', '2', 1);
+(6, 'Orm', 'orm@gmail.com', '77777777777', '6666666666666', '(77)77777-7777', '2', 1),
+(7, 'Orm', 'orm@fatec.sp.gov.br', '44444444444', '4444444444444', '(44)44444-4444', '1', 1),
+(8, 'julio', 'julio@gmail.com', '88888888888', NULL, '(88)88888-8888', '2', 1),
+(9, 'eduarda', 'eduarda@fatec.sp.gov.br', '99999999999', '9999999999999', '(99)99999-9999', '0', 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `registros`
+-- Estrutura para tabela `registros_entradas`
 --
 
-CREATE TABLE `registros` (
+CREATE TABLE `registros_entradas` (
   `id` int(11) NOT NULL,
-  `data_entrada` date NOT NULL,
-  `horario_entrada` time NOT NULL,
-  `data_saida` date NOT NULL,
-  `horario_saida` time NOT NULL,
-  `veiculos_id` int(11) NOT NULL
+  `data_entrada` datetime NOT NULL,
+  `horario_entrada` datetime NOT NULL,
+  `veiculos_id` int(11) NOT NULL,
+  `usuarios_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `registros_entradas`
+--
+
+INSERT INTO `registros_entradas` (`id`, `data_entrada`, `horario_entrada`, `veiculos_id`, `usuarios_id`) VALUES
+(2, '2024-11-13 00:00:00', '2024-11-13 19:54:00', 2, 1),
+(3, '2024-11-13 00:00:00', '2024-11-13 20:04:00', 1, 1),
+(4, '2024-11-13 00:00:00', '2024-11-13 20:08:00', 1, 1),
+(5, '2024-11-13 00:00:00', '2024-11-13 20:08:00', 1, 1),
+(6, '2024-11-13 00:00:00', '2024-11-13 20:08:00', 1, 1),
+(7, '2024-11-13 00:00:00', '2024-11-13 20:16:00', 1, 1),
+(8, '2024-11-13 00:00:00', '2024-11-13 20:20:00', 1, 1),
+(9, '2024-11-13 00:00:00', '2024-11-13 20:21:00', 2, 1),
+(10, '2024-11-13 00:00:00', '2024-11-13 20:22:00', 1, 1),
+(11, '2024-11-13 00:00:00', '2024-11-13 20:36:00', 1, 1),
+(12, '2024-11-13 00:00:00', '2024-11-13 20:37:00', 2, 1),
+(13, '2024-11-13 00:00:00', '2024-11-13 20:51:00', 1, 1),
+(14, '2024-11-13 00:00:00', '2024-11-13 20:51:00', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `registros_saida`
+--
+
+CREATE TABLE `registros_saida` (
+  `id` int(11) NOT NULL,
+  `data_saida` datetime NOT NULL,
+  `horario_saida` datetime NOT NULL,
+  `modelo` varchar(30) NOT NULL,
+  `veiculos_id` int(11) NOT NULL,
+  `usuarios_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -96,19 +139,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `nivel_acesso`) VALUES
-(1, 'Eduarda Pontes', 'eduarda.pontes@fatec.sp.gov.br', '1234', 0);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `usuarios_registros`
---
-
-CREATE TABLE `usuarios_registros` (
-  `id` int(11) NOT NULL,
-  `usuarios_id` int(11) NOT NULL,
-  `registros_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+(1, 'Eduarda Pontes', 'eduarda.pontes@fatec.sp.gov.br', '1234', 0),
+(2, 'Folk ', 'folk@gmail.com', '1234', 1),
+(3, 'Dawan', 'dawan@fatec.sp.gov.br', '1234', 0);
 
 -- --------------------------------------------------------
 
@@ -131,7 +164,9 @@ CREATE TABLE `veiculos` (
 --
 
 INSERT INTO `veiculos` (`id`, `placa`, `tipo`, `modelo`, `cor`, `motoristas_id`, `usuarios_id`) VALUES
-(1, '11111111', 'Moto', 'Honda 150', 'Preta', 1, 1);
+(1, '11111111', 'Moto', 'Honda 150', 'Preta', 1, 1),
+(2, '44444444', 'Moto', 'Honda 160', 'Vermelha', 7, 1),
+(3, '99999999', 'Carro', 'BMW', 'Branca', 9, 1);
 
 --
 -- Índices para tabelas despejadas
@@ -152,11 +187,20 @@ ALTER TABLE `motoristas`
   ADD KEY `usuarios_id` (`usuarios_id`);
 
 --
--- Índices de tabela `registros`
+-- Índices de tabela `registros_entradas`
 --
-ALTER TABLE `registros`
+ALTER TABLE `registros_entradas`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `veiculos_id` (`veiculos_id`);
+  ADD KEY `veiculos_id` (`veiculos_id`),
+  ADD KEY `usuarios_id` (`usuarios_id`);
+
+--
+-- Índices de tabela `registros_saida`
+--
+ALTER TABLE `registros_saida`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `veiculos_id` (`veiculos_id`),
+  ADD KEY `usuarios_id` (`usuarios_id`);
 
 --
 -- Índices de tabela `usuarios`
@@ -164,14 +208,6 @@ ALTER TABLE `registros`
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
-
---
--- Índices de tabela `usuarios_registros`
---
-ALTER TABLE `usuarios_registros`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `usuarios_id` (`usuarios_id`),
-  ADD KEY `registros_id` (`registros_id`);
 
 --
 -- Índices de tabela `veiculos`
@@ -190,37 +226,37 @@ ALTER TABLE `veiculos`
 -- AUTO_INCREMENT de tabela `estacionamento`
 --
 ALTER TABLE `estacionamento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `motoristas`
 --
 ALTER TABLE `motoristas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT de tabela `registros`
+-- AUTO_INCREMENT de tabela `registros_entradas`
 --
-ALTER TABLE `registros`
+ALTER TABLE `registros_entradas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de tabela `registros_saida`
+--
+ALTER TABLE `registros_saida`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de tabela `usuarios_registros`
---
-ALTER TABLE `usuarios_registros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `veiculos`
 --
 ALTER TABLE `veiculos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restrições para tabelas despejadas
@@ -233,17 +269,18 @@ ALTER TABLE `motoristas`
   ADD CONSTRAINT `motoristas_ibfk_1` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`);
 
 --
--- Restrições para tabelas `registros`
+-- Restrições para tabelas `registros_entradas`
 --
-ALTER TABLE `registros`
-  ADD CONSTRAINT `registros_ibfk_1` FOREIGN KEY (`veiculos_id`) REFERENCES `veiculos` (`id`);
+ALTER TABLE `registros_entradas`
+  ADD CONSTRAINT `registros_entradas_ibfk_1` FOREIGN KEY (`veiculos_id`) REFERENCES `veiculos` (`id`),
+  ADD CONSTRAINT `registros_entradas_ibfk_2` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`);
 
 --
--- Restrições para tabelas `usuarios_registros`
+-- Restrições para tabelas `registros_saida`
 --
-ALTER TABLE `usuarios_registros`
-  ADD CONSTRAINT `usuarios_registros_ibfk_1` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`),
-  ADD CONSTRAINT `usuarios_registros_ibfk_2` FOREIGN KEY (`registros_id`) REFERENCES `registros` (`id`);
+ALTER TABLE `registros_saida`
+  ADD CONSTRAINT `registros_saida_ibfk_1` FOREIGN KEY (`veiculos_id`) REFERENCES `veiculos` (`id`),
+  ADD CONSTRAINT `registros_saida_ibfk_2` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`);
 
 --
 -- Restrições para tabelas `veiculos`
